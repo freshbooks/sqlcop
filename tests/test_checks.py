562335de12b0742228c88535198a2e42c670081d
Kevin Qiu <kevin@freshbooks.com>
2014-12-19 10:51:55 -0500

    Fix tests

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,95 +1,109 @@
 import sqlparse
+import contextlib
 from sqlcop.checks import CrossJoinCheck
-from mock import Mock
+from mock import Mock, patch, PropertyMock
 
 
 class TestCheckCrossJoin(object):
     def setup_method(self, method):
         self.has_cross_join = CrossJoinCheck()
 
+    @contextlib.contextmanager
+    def patch_schema(self, table_schema):
+        ctx_mgr = patch(
+            'sqlcop.checks.CrossJoinCheck.tables', new_callable=PropertyMock)
+        tables = ctx_mgr.__enter__()
+        tables.__get__ = Mock(return_value=table_schema)
+        yield
+        ctx_mgr.__exit__()
+
     def test_no_cross_join(self):
-        self.has_cross_join.tables = {}
-        sql = (
-            "SELECT * FROM a LEFT JOIN b "
-            "USING (id)"
-        )
-        stmt = sqlparse.parse(sql)[0]
-        assert False == self.has_cross_join(stmt)
+        with self.patch_schema({}):
+            sql = (
+                "SELECT * FROM a LEFT JOIN b "
+                "USING (id)"
+            )
+            stmt = sqlparse.parse(sql)[0]
+            assert False == CrossJoinCheck()(stmt)
 
     def test_has_cross_join(self):
         tbl_a, tbl_b = Mock(), Mock()
         tbl_a.primary_key.columns.keys.return_value = ['id']
         tbl_b.primary_key.columns.keys.return_value = ['id']
-        self.has_cross_join.tables = {
+        schema = {
             'a': tbl_a,
             'b': tbl_b,
         }
-        sql = (
-            "SELECT * FROM a, b WHERE a.c = 1"
-        )
-        stmt = sqlparse.parse(sql)[0]
-        assert True == self.has_cross_join(stmt)
+        with self.patch_schema(schema):
+            sql = (
+                "SELECT * FROM a, b WHERE a.c = 1"
+            )
+            stmt = sqlparse.parse(sql)[0]
+            assert True == self.has_cross_join(stmt)
 
     def test_no_cross_join_with_reserved_words(self):
-        self.has_cross_join.tables = {}
-        sql = (
-            "SELECT * FROM user LEFT JOIN system "
-            "USING (id)"
-        )
-        stmt = sqlparse.parse(sql)[0]
-        assert False == self.has_cross_join(stmt)
+        with self.patch_schema({}):
+            sql = (
+                "SELECT * FROM user LEFT JOIN system "
+                "USING (id)"
+            )
+            stmt = sqlparse.parse(sql)[0]
+            assert False == self.has_cross_join(stmt)
 
     def test_no_cross_join_if_join_in_where_clause(self):
         tbl_a, tbl_b = Mock(), Mock()
         tbl_a.primary_key.columns.keys.return_value = ['id']
         tbl_b.primary_key.columns.keys.return_value = ['id']
-        self.has_cross_join.tables = {
+        schema = {
             'a': tbl_a,
             'b': tbl_b,
         }
-        sql = (
-            "SELECT * FROM a, b "
-            "WHERE 1 = a.type "
-            "AND 'b' = b.activity "
-            "AND a.id = b.id "
-        )
-        stmt = sqlparse.parse(sql)[0]
-        assert False == self.has_cross_join(stmt)
+        with self.patch_schema(schema):
+            sql = (
+                "SELECT * FROM a, b "
+                "WHERE 1 = a.type "
+                "AND 'b' = b.activity "
+                "AND a.id = b.id "
+            )
+            stmt = sqlparse.parse(sql)[0]
+            assert False == self.has_cross_join(stmt)
 
     def test_has_cross_join_if_not_all_keys_in_join(self):
         tbl_a, tbl_b = Mock(), Mock()
         tbl_a.primary_key.columns.keys.return_value = ['x_id', 'y_id']
         tbl_b.primary_key.columns.keys.return_value = ['x_id', 'y_id']
-        self.has_cross_join.tables = {
+        schema = {
             'a': tbl_a,
             'b': tbl_b,
         }
-        sql = (
-            "SELECT * FROM a, b "
-            "WHERE 1 = a.type "
-            "AND 'b' = b.activity "
-            "AND a.x_id = b.x_id "
-        )
-        stmt = sqlparse.parse(sql)[0]
-        assert True == self.has_cross_join(stmt)
+        with self.patch_schema(schema):
+            sql = (
+                "SELECT * FROM a, b "
+                "WHERE 1 = a.type "
+                "AND 'b' = b.activity "
+                "AND a.x_id = b.x_id "
+            )
+            stmt = sqlparse.parse(sql)[0]
+            assert True == self.has_cross_join(stmt)
 
     def test_with_placeholders(self):
         tbl_ps, tbl_user = Mock(), Mock()
         tbl_ps.primary_key.columns.keys.return_value = ['systemid', 'staffid']
         tbl_user.primary_key.columns.keys.return_value = ['systemid', 'userid']
-        self.has_cross_join.tables = {
+        schema = {
             'user': tbl_user,
             'ps': tbl_ps,
         }
-        sql = (
-            "SELECT * FROM user, ps "
-            "WHERE %s = ps.projectid "
-            "AND %s = ps.systemid "
-            "AND ps.systemid = user.systemid "
-            "AND ps.staffid = user.userid "
-            "AND user.level = %s "
-            "AND user.active = %s "
-            "ORDER BY user.userid ASC"
-        )
-        stmt = sqlparse.parse(sql)[0]
-        assert False == self.has_cross_join(stmt)
+        with self.patch_schema(schema):
+            sql = (
+                "SELECT * FROM user, ps "
+                "WHERE %s = ps.projectid "
+                "AND %s = ps.systemid "
+                "AND ps.systemid = user.systemid "
+                "AND ps.staffid = user.userid "
+                "AND user.level = %s "
+                "AND user.active = %s "
+                "ORDER BY user.userid ASC"
+            )
+            stmt = sqlparse.parse(sql)[0]
+            assert False == self.has_cross_join(stmt)
