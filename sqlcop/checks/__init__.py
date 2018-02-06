679e3b17a750f72dc0b507af73dce8aed2cab922
achaudhury <achaudhury@freshbooks.com>
2018-02-05 22:36:40 -0500

    Run futurize on codebase and update dependencies

--- sqlcop/checks/__init__.py
+++ sqlcop/checks/__init__.py
@@ -4,8 +4,9 @@ import sqlparse
 def _patch():
     from sqlalchemy.dialects.mysql.base import RESERVED_WORDS
 
-    for key in sqlparse.keywords.KEYWORDS.keys():
+    for key in list(sqlparse.keywords.KEYWORDS.keys()):
         if key not in RESERVED_WORDS:
             del sqlparse.keywords.KEYWORDS[key]
+
 
 _patch()
